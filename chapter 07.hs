fabbb8f5f5a12c526190506c2654df5e38e34e59
oliveira-caio <51940926+oliveira-caio@users.noreply.github.com>
2021-03-20 22:33:25 -0300

    Update and rename 7.hs to chapter 07.hs

--- chapter 07.hs
+++ chapter 07.hs
@@ -1,7 +1,4 @@
-{- Exercicio 1 
-map . filter -}
-
-{- Exercicio 2 -}
+{- Exercise 2 -}
 all' :: (a -> Bool) -> [a] -> Bool
 all' _ [] = True
 all' p (x:xs) = p x && all' p xs
@@ -22,36 +19,33 @@ dropWhile' _ [] = []
 dropWhile' p (x:xs) | p x       = dropWhile' p xs
                     | otherwise = [x] ++ xs
 
-{- Exercicio 3 -}
+
+
+
+
+{- Exercise 3 -}
 map' :: (a -> b) -> [a] -> [b]
 map' f = foldr (\x xs -> f x : xs) []
 
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' p = foldr (\x xs -> if p x then x:xs else xs) []
 
-{- Exercicio 5 -}
-dec2int :: [Int] -> Int
-dec2int ns = foldl (\x y -> 10*x + y) 0 ns
 
-{- foldr' :: (a -> b -> b) -> b -> [a] -> b
-foldr' f v [] = v
-foldr' f v (x:xs) = f x (foldr' f v xs)
 
-foldl' :: (b -> a -> b) -> b -> [a] -> b
-foldl' f v [] = v
-foldl' f v (x:xs) = foldl' f (f v x) xs
 
-foldl' (\x y -> 10*x + y) 11 [2]
-foldl' (\x y -> 10*x + y) (110 + 2) [] -}
 
-{- Exercicio 5 -}
+{- Exercise 5 -}
 curry :: ((a,b) -> c) -> (a -> b -> c)
 curry f x y = f (x,y)
 
 uncurry :: (a -> b -> c) -> ((a,b) -> c)
 uncurry f p = f (fst p) (snd p)
 
-{- Exercicio 6 -}
+
+
+
+
+{- Exercise 6 -}
 type Bit = Int
 
 unfold p h t x | p x       = []
@@ -64,12 +58,16 @@ map2 f = unfold null (f.head) tail
 
 iterate2 f = unfold (\x -> False) f f
 
-{- Exercicio 9 -}
+
+
+
+
+{- Exercise 9 -}
 altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 altMap _ _ [] = []
 altMap f g (x:y:xs) = (f x) : (g y) : altMap f g xs
 
-{- Exercicio 10 -}
+{- Exercise 10 -}
 luhnDouble :: Int -> Int
 luhnDouble x = if 2*x < 10 then 2*x else 2*x - 9
 
