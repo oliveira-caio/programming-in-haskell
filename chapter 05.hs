a69d1e1d509d177685a2e5cb9510972aba127006
oliveira-caio <51940926+oliveira-caio@users.noreply.github.com>
2021-03-20 22:29:52 -0300

    Update and rename 5.hs to chapter 05.hs

--- chapter 05.hs
+++ chapter 05.hs
@@ -1,30 +1,30 @@
-{- Exercicio 1 -}
+{- Exercise 1 -}
 sumsquare :: Int -> Int
 sumsquare n = sum [i*i | i <- [1..n]]
 
-{- Exercicio 2 -}
+{- Exercise 2 -}
 grid :: Int -> Int -> [(Int,Int)]
 grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
 
-{- Exercicio 3 -}
+{- Exercise 3 -}
 square :: Int -> [(Int,Int)]
 square n = [(x,y) | x <- [0..n], y <- [0..n], x /= y]
 
-{- Exercicio 4 -}
+{- Exercise 4 -}
 replicate' :: Int -> a -> [a]
 replicate' n x = [x | i <- [0..(n-1)]]
 
-{- Exercicio 5 -}
+{- Exercise 5 -}
 pyths :: Int -> [(Int,Int,Int)]
 pyths n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], x*x + y*y == z*z]
 
-{- Exercicio 6 -}
+{- Exercise 6 -}
 factors :: Int -> [Int]
 factors n = [x | x <- [1..n], n `mod` x == 0]
 
 perfects :: Int -> [Int]
 perfects n = [x | x <- [1..n], x == (sum (factors x) - x)]
 
-{- Exercicio 9 -}
+{- Exercise 9 -}
 scalarproduct :: [Int] -> [Int] -> Int
 scalarproduct x y = sum [fst k * snd k | k <- zip x y]
